37a57ca839ba8b90905997ff5f73015f87ce9c28
futabooo <takahiro.futagawa@gmail.com>
2023-01-14 12:15:34 +0900

    wip

--- lib/functions.dart
+++ lib/functions.dart
@@ -1,5 +1,43 @@
 import 'package:functions_framework/functions_framework.dart';
+import 'package:googleapis/vision/v1.dart';
+import 'package:googleapis_auth/auth_io.dart';
 import 'package:shelf/shelf.dart';
 
 @CloudFunction()
-Response function(Request request) => Response.ok('Hello, World!');
+Future<Response> function(Request request) async {
+  final params = request.url.queryParameters;
+  if (!params.keys.contains('url')) {
+    return Response.badRequest();
+  }
+  // final imageUrl = params['url'];
+  final imageUrl =
+      'https://pbs.twimg.com/media/FmXCf54aAAEQdWQ?format=jpg&name=large';
+  print('imageUrl = $imageUrl');
+
+  final client = await clientViaApplicationDefaultCredentials(
+    scopes: [VisionApi.cloudVisionScope],
+  );
+  final visionApi = VisionApi(client);
+  final response = await visionApi.images.annotate(
+    BatchAnnotateImagesRequest.fromJson({
+      "requests": [
+        {
+          "image": {
+            "source": {"imageUri": "$imageUrl"}
+          },
+          "features": [
+            {"type": "TEXT_DETECTION"}
+          ]
+        }
+      ]
+    }),
+  );
+
+  print(response.toJson().toString());
+
+  final textes = response.responses
+          ?.map((e) => e.fullTextAnnotation?.text ?? '')
+          .toList() ??
+      [];
+  return Response.ok('success ${textes.join()}');
+}
